2fcb1b9d71062526e9a71e4d49bd1e0810db8505
Lucas Farah <lucas.farah@me.com>
2016-05-10 16:24:39 -0700

    heroku

--- Kodeo.py
+++ Kodeo.py
@@ -46,5 +46,6 @@ def api_hello():
 				points += 2
 	return "points: " + str(points)
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
