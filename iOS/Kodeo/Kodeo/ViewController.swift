005f4282e3c0084dc1b51db9dda1ed35efbc9546
Lucas Farah <lucas.farah@me.com>
2016-05-19 19:24:32 -0700

    Navigation bar + Refresh button

--- iOS/Kodeo/Kodeo/ViewController.swift
+++ iOS/Kodeo/Kodeo/ViewController.swift
@@ -19,13 +19,23 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 
+		fetchUsers()
+	}
+
+	func fetchUsers() {
 		let names = ["lfarah", "krausefx", "dkhamsing", "troydo42"]
 		UserManager().fetchUsers(names) { (users) in
 			self.arrayUsers = users
 			self.table.reloadData()
 		}
+
 	}
 
+	@IBAction func butRefresh(sender: AnyObject) {
+
+    fetchUsers()
+	}
+  
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
@@ -51,22 +61,22 @@ extension ViewController: UITableViewDataSource {
 		let user = arrayUsers[indexPath.row]
 
 		cell.lblNameUser.text = user.name
-    
-    var points = "\(user.totalPoints)".bold()
-    points += NSAttributedString(string: " points")
-    
-    cell.lblPointsUser.attributedText = points
-    cell.imgvUser.image = user.userPic
-    
-    let ranking = indexPath.row + 1
-    
-    if ranking == 1 {
-      cell.lblRankingUser.backgroundColor = UIColor(red: 245/255, green: 166/255, blue: 35/255, alpha: 1)
-    }
-    
-    cell.lblRankingUser.text = "\(ranking)"
-    
-    return cell
+
+		var points = "\(user.totalPoints)".bold()
+		points += NSAttributedString(string: " points")
+
+		cell.lblPointsUser.attributedText = points
+		cell.imgvUser.image = user.userPic
+
+		let ranking = indexPath.row + 1
+
+		if ranking == 1 {
+			cell.lblRankingUser.backgroundColor = UIColor(red: 245 / 255, green: 166 / 255, blue: 35 / 255, alpha: 1)
+		}
+
+		cell.lblRankingUser.text = "\(ranking)"
+
+		return cell
 	}
 }
 
