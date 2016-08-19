6c2be7b718b5762ffbb5ac0c815aebf12a6ec754
Lucas Farah <lucas.farah@me.com>
2016-08-18 21:30:19 -0700

    Still trying

--- Backend/Kodeo.py
+++ Backend/Kodeo.py
@@ -13,53 +13,6 @@ app = Flask(__name__)
 def firstPage():
 	return "Hello World"
 
-def get_contributions_for_user(username):
-  content = requests.get("https://github.com/users/%s/contributions" % username).text
-
-  lines = content.splitlines()
-  lines = [x.strip() for x in lines]
-  lines = [x for x in lines if x.startswith('<rect class="day"')]
-
-  return lines
-
-# returns YYYY-MMMM-DD strings for each day with a contribution
-def get_contribution_days_for_user(username):
-  data = get_contributions_for_user(username)
-  data = [x[-13:-3] for x in data if "#eeeeee" not in x]
-  return data
-
-def get_streak_for_user(username):
-  data = get_contributions_for_user(username)
-
-  contribs = []
-  offset = len("data-count=")
-  for line in data:
-    idx = line.find("data-count=") + offset + 1
-    line = line[idx:]
-    parts = line.split('"')
-    count = int(parts[0])
-    date = datetime.strptime(parts[2], "%Y-%m-%d")
-    contribs.append((count, date))
-
-  if not contribs:
-    return 'error'
-
-  # remove dates in the future
-  cur = contribs.pop()
-  while cur[1] >= datetime.today():
-    cur = contribs.pop()  
-
-  if not contribs:
-      return 'error'
-
-  # count current streak
-  streak = 0
-  while cur[0] != 0:
-    streak += 1
-    cur = contribs.pop()
-
-  return streak
-  
 @app.route('/getpointsForUser')
 def api_hello():
 	dic = {"username": "", "userPic": "", "totalPoints":0,"dailyPoints":[],"PushEvent":0,"PullRequestEvent":0,"IssueCommentEvent":0,"IssueEvent":0, "CommitEvent": 0}
@@ -129,7 +82,31 @@ def api_hello():
 
 	dic["totalPoints"] = points
 	dic["dailyPoints"] = dailyPoints
-	dic["ContributionStreak"] = get_streak_for_user(user)
+
+	content = requests.get("https://github.com/users/%s/contributions" % user).text
+	lines = content.splitlines()
+	lines = [x.strip() for x in lines]
+	lines = [x for x in lines if x.startswith('<rect class="day"')]
+	contribs = []
+	offset = len("data-count=")
+	for line in lines:
+		idx = line.find("data-count=") + offset + 1
+		line = line[idx:]
+		parts = line.split('"')
+		count = int(parts[0])
+		date = datetime.strptime(parts[2], "%Y-%m-%d")
+		contribs.append((count, date))
+
+	cur = contribs.pop()
+	while cur[1] >= datetime.today():
+		cur = contribs.pop()  
+
+	streak = 0
+	while cur[0] != 0:
+		streak += 1
+		cur = contribs.pop()
+
+	dic["ContributionStreak"] = streak
 
 	print dailyPoints
 	print points
